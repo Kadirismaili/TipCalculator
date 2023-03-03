e6016d8b22354b281ca324ed7df6dbb2d2ab4f8e
Fly <kiwipromm@gmail.com>
2023-03-03 10:05:42 +0100

    Initial Commit

---
+++ TestProject1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestProject1
+//
+//  Created by KADIR ISMAILI on 3.3.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
